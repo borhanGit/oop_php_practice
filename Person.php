7d6c4fca6023252ce4a6e35bbc409d48d985dc21
borhanGit <mdborhan413@gmail.com>
2021-10-25 12:41:33 +0600

    destruct is a another magic method that works end of this class instance

--- Person.php
+++ Person.php
@@ -2,6 +2,7 @@
 class Person {
     public $name;
     public $age;
+    public $id;
     public function __construct($name,$age)
     {
         $this->name = $name;
@@ -12,8 +13,18 @@ class Person {
     {
         echo "The person name is {$this->name} and the person age is {$this->age}";
     }
+    function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function __destruct()
+    {
+        echo "Saving..";
+    }
 }
 
 $personOne = new Person("Borhan Uddin","28");
-$personOne->personDetails();
+$personOne->setId(12);
+unset($personOne);
+
 ?>
